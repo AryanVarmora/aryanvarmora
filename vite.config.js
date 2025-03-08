e919900fe7d6d1da48de70317655477e61633b68
AryanVarmora <aryanvarmora66@gmail.com>
2025-03-08 16:17:24 -0500

    Fix: Clean build & correct asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative path for GitHub Pages to correctly load assets
+  base: '', // No `/assets/` path issue
 });
