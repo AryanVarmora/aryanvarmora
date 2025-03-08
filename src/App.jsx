aee686de14ca471bcadf16d4a48fd74d7c09d931
AryanVarmora <aryanvarmora66@gmail.com>
2025-03-08 16:33:55 -0500

    Added 3D scene with Three.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas>
+        {/* Lights */}
         <ambientLight intensity={0.5} />
         <directionalLight position={[1, 2, 3]} />
+        
+        {/* Interactive 3D Object */}
         <OrbitControls />
         <Sphere args={[1, 32, 32]} position={[0, 0, 0]}>
           <meshStandardMaterial attach="material" color="hotpink" />
