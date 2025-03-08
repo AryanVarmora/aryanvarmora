17f387de6ae69fbd8aa0f797ee5ab0d487fdd282
AryanVarmora <aryanvarmora66@gmail.com>
2025-03-08 16:11:46 -0500

    Move index.html to root for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensures correct routing for GitHub Pages
+  base: '/',  // Ensures correct path
 });
